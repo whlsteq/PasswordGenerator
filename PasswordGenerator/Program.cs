828504a9ae7af71ad597dd27d5f3cdf50e738c0d
Utku <utku.canal@avkaryazilim.com.tr>
2022-01-12 22:54:24 +0300

    Add project files.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+String chars= "$%#@!*abcdefghijklmnopqrstuvwxyz1234567890?;:ABCDEFGHIJKLMNOPQRSTUVWXYZ^&";
+String password = "";
+for (int i = 0; i < 12; i++)
+{
+    Random random = new Random();
+    random.Next(0, chars.Length);
+    int num = random.Next(0, chars.Length);
+    password += chars[num].ToString();
+}
+Console.WriteLine(password);
+
